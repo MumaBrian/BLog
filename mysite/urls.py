d2afaef9186a81d83c2bfe5c5972729d0a9e63fd
MumaBrian <brianmuma5@gmail.com>
2022-04-23 07:04:13 -0400

    git

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,11 +5,14 @@ from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
 
 sitemaps={
-    'posts':PostSitemap
+    'posts':PostSitemap,
 }
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls',namespace='blog')),
+    
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),    
 ]
 
